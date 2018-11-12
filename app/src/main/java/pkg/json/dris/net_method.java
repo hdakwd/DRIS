f8b977b8782232a0ccee6a9a1c74db51657f836c
hdakwd <egotism49@gmail.com>
2018-11-12 20:43:33 +0900

    ドライブレコーダの映像を提供するシステム。 ネット接続の方法次第でその名前を表示する。

--- app/src/main/java/pkg/json/dris/net_method.java
+++ app/src/main/java/pkg/json/dris/net_method.java
@@ -3,6 +3,10 @@ package pkg.json.dris;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
+import static android.content.Context.WIFI_SERVICE;
 
 public class net_method {
     ConnectivityManager cm;
@@ -16,22 +20,27 @@ public class net_method {
     }
 
     public String net_status() {
-
         cm =  (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
         nInfo = cm.getActiveNetworkInfo();
+
+        WifiManager manager = (WifiManager)context.getSystemService(WIFI_SERVICE);
+        WifiInfo info = manager.getConnectionInfo();
         String status = "null";
 
         if (nInfo == null) {
             status = "disconnect";
         }else if (nInfo.isConnected()) {
             if (nInfo.getTypeName().equals("WIFI")) {
-                status = "Wi-Fi";
+                if(info.getSSID().equals("drireco")) {
+                    status = "drireco";
+                }else {
+                    status = "Wi-Fi";
+                }
             } else if (nInfo.getTypeName().equals("mobile")) {
                 status = "connect";
             }else {
                 status = "What?";
             }
-
         } else {
             status = "disconnect";
         }
